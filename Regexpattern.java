254459b49c8dcbc52751c80f80ce33bc79257ecb
nikita1803 <rainikita595@gmail.com>
2021-05-19 19:42:54 +0530

    Added: Pin Code is valid or not

--- Regexpattern.java
+++ Regexpattern.java
@@ -1,8 +1,20 @@
 package regexpattern;
 
-public class Regexpattern {
-	public static void main(String[] args) {
-		System.out.println("Welcome in Regex pattern validation");
-	}
+import java.util.regex.*;
 
+import java.util.Scanner;
+
+public class Regexpattern {
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome in Regex pattern validation");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the pincode");
+		String pincode = sc.nextLine();
+		sc.nextLine();
+		String regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
+		System.out.println("The zip code is: " + pincode);
+	    System.out.println("Is the pin code is correct? " + pincode.matches(regex));  
+	}
 }
